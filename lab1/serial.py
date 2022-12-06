cb8219f501d2175aab0ee58c36ca7e6d9cd798d9
Ramneet-Singh <ramneet2001@gmail.com>
2022-12-06 16:07:26 +0530

    serial done

---
+++ lab1/serial.py
@@ -0,0 +1,20 @@
+import re
+
+def clean(inp):
+	inp = inp.lower()
+	inp = re.sub(r'[^a-z ]','',inp)
+	return inp.split()
+
+import os 
+
+count = {}
+
+for f in os.listdir('data/'):
+	print(f)
+	data = clean(open(os.path.join(os.path.dirname(__file__), 'data', f),'r').read())
+	for w in data:
+		if w not in count:
+			count[w] = 0
+		count[w] += 1
+print((sorted(count.items(), key=lambda item: item[1],reverse=True)[:10]))
+
